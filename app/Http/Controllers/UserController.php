5e459db5e145135190bc875a16bf734d43da47b5
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-06-01 19:22:32 -0700

    11. CREATED CONDITIONAL STATEMENT TO IF AN IMAGE IS UPLOADED FOR  AVATAR resources/views/layouts/app.blade.php
    @if(Auth::user()->avatar)
      <img src={{asset(/storage/images/.Auth::user()->avatar)}} alt=avatar width=40/>
    @endif
    
    12.    CREATED CONDITIONAL STATEMENT TO DELETE  IMAGE WHEN UPDATED IN UPLOAD AVATAR FUNCTION OF app/http/controllers/UserController.php
    
     // This is from vendor\laravel\framework\src\Illuminate\Http\UploadedFile.php  search for getClientOriginalName
                 = ->image->getClientOriginalName();
                if (auth()->user()->avatar)
                {
                    // This will delete the image in
                    Storage::delete(/public/images/ . auth()->user()->avatar);
                }
                ->image->storeAs(images, , public);
                auth()->user()->update([avatar=> ]);
                // User::find(1)->update([avatar => ]);

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -17,9 +17,14 @@ class UserController extends Controller
         if ($request->hasFile('image')) {
             // This is from vendor\laravel\framework\src\Illuminate\Http\UploadedFile.php  search for getClientOriginalName 
             $filename = $request->image->getClientOriginalName();
+            if (auth()->user()->avatar)
+            {
+                // This will delete the image in 
+                Storage::delete('/public/images/' . auth()->user()->avatar);
+            }
             $request->image->storeAs('images', $filename, 'public');
+            auth()->user()->update(['avatar'=> $filename]);
             // User::find(1)->update(['avatar' => $filename]);
-            auth()->user()->update(['avatar' => $filename]);
 
         }
         return redirect()->back();
