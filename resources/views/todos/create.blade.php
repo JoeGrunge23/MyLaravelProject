62559692cc8fea01b5960b5884cfe4f2ee2f0b78
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-06-06 06:59:23 +0800

    updated

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,3 +1,12 @@
  @extends('todos.layout')
 
  @section('content')
+
+ <h1 class="text-2x1">What next you need To-DO</h1>
+    <x-alert />
+    <form action="/todos/create" method="post" class="py-5">
+        @csrf 
+        <input type="text" name="title" class="py-2 px-2 border rounded"/>
+        <input type="submit" value="Create" class="p-2 border rounded">
+    </form>
+@endsection    
