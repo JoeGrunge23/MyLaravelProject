37bc188bea46859c9ff00cd44ca64823d39d7291
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-06-06 06:54:35 +0800

    updated

--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -10,14 +10,7 @@
 </head>
 <body>
     <div class="text-center pt-10">
-        <h1 class="text-2x1">All Your Todos</h1>
-        <ul>
-        @foreach($todos as $todo)
-            <li>{{$todo->title}}</li>
-        @endforeach
-        </ul>
-
+        @yield('content');
     </div>
 </body>
 </html>
-
